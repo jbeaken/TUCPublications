1a6a666b0687c23d986c07a1a4115e90aba8adac
jbeaken <jack747@gmail.com>
2015-04-02 12:36:11 +0100

    Removing apostraphe

--- src/main/java/org/bookmarks/ui/comparator/GardnersDeliveryComparator.java
+++ src/main/java/org/bookmarks/ui/comparator/GardnersDeliveryComparator.java
@@ -8,17 +8,26 @@ public class GardnersDeliveryComparator implements Comparator {
 	public int compare(Object obj1, Object obj2){
 		SupplierDeliveryLine sdl1 = (SupplierDeliveryLine)obj1;
 		SupplierDeliveryLine sdl2 = (SupplierDeliveryLine)obj2;
-		String title1 = removeTheAndAnd(sdl1.getStockItem().getTitle().toLowerCase());
-		String title2 = removeTheAndAnd(sdl2.getStockItem().getTitle().toLowerCase());
+
+		String title1 = prepare(sdl1.getStockItem().getTitle().toLowerCase());
+		String title2 = prepare(sdl2.getStockItem().getTitle().toLowerCase());
+		
 		return title1.compareTo(title2);
 	}
 
-	private String removeTheAndAnd(String s) {
+/**
+* Remove leading The or 'a'
+* Remove apostraphes (I've -> Ive)
+**/
+	private String prepare(String s) {
+
 		if(s.startsWith("the ")){
-			return s.substring(4);
+			s = s.substring(4);
 		} else if(s.startsWith("a ")){
 			return s.substring(2);
 		}
+
+		s = s.replace("\'", "");
 		return s;
 	}
 }
