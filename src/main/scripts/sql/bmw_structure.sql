da27f4d9e4426c9e3804ca5d71f16a28cd42222a
jbeaken <jack747@gmail.com>
2015-04-07 11:58:39 +0100

    Moving build bookmarks to bmw sql scripts from bmw to bookmarks
    Former-commit-id: 512b29ba0d4151ae14bddd26926a2f581ae7bb5b

---
+++ src/main/scripts/sql/bmw_structure.sql
@@ -0,0 +1,398 @@
+-- MySQL dump 10.13  Distrib 5.5.41, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: bmw
+-- ------------------------------------------------------
+-- Server version	5.5.41-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `author`
+--
+
+DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `author` (
+  `id` bigint(20) NOT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_or6k6jmywerxbme223c988bmg` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `author`
+--
+
+LOCK TABLES `author` WRITE;
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date_created` datetime DEFAULT NULL,
+  `is_in_sidebar` bit(1) DEFAULT NULL,
+  `is_on_website` bit(1) DEFAULT NULL,
+  `name` varchar(30) NOT NULL,
+  `parent_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_81thrbnb8c08gua7tvqj7xdqk` (`parent_id`),
+  CONSTRAINT `FK_81thrbnb8c08gua7tvqj7xdqk` FOREIGN KEY (`parent_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category_categories`
+--
+
+DROP TABLE IF EXISTS `category_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category_categories` (
+  `category` bigint(20) NOT NULL,
+  `categories` bigint(20) NOT NULL,
+  PRIMARY KEY (`category`,`categories`),
+  UNIQUE KEY `UK_7um9d6vh8tpm6caj3ics9jjax` (`categories`),
+  CONSTRAINT `FK_s52fgdy2v2gjx1b795j9l9ua6` FOREIGN KEY (`category`) REFERENCES `category` (`id`),
+  CONSTRAINT `FK_7um9d6vh8tpm6caj3ics9jjax` FOREIGN KEY (`categories`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category_categories`
+--
+
+LOCK TABLES `category_categories` WRITE;
+/*!40000 ALTER TABLE `category_categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date_created` datetime DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_gplwv1k6khglovl9nsjmc0ai1` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date_created` datetime DEFAULT NULL,
+  `address1` varchar(255) DEFAULT NULL,
+  `address2` varchar(255) DEFAULT NULL,
+  `address3` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `postcode` varchar(255) DEFAULT NULL,
+  `beans_id` bigint(20) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `home_number` varchar(255) DEFAULT NULL,
+  `mobile_number` varchar(255) DEFAULT NULL,
+  `work_number` varchar(255) DEFAULT NULL,
+  `credit_card1` varchar(255) DEFAULT NULL,
+  `credit_card2` varchar(255) DEFAULT NULL,
+  `credit_card3` varchar(255) DEFAULT NULL,
+  `credit_card4` varchar(255) DEFAULT NULL,
+  `expiry_month` varchar(255) DEFAULT NULL,
+  `expiry_year` varchar(255) DEFAULT NULL,
+  `name_on_card` varchar(255) DEFAULT NULL,
+  `security_code` varchar(255) DEFAULT NULL,
+  `delivery_type` varchar(255) NOT NULL,
+  `first_name` varchar(55) NOT NULL,
+  `has_been_consumed` bit(1) NOT NULL,
+  `last_name` varchar(55) NOT NULL,
+  `payment_type` varchar(255) NOT NULL,
+  `pending_consumption` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `event`
+--
+
+DROP TABLE IF EXISTS `event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `event` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date_created` datetime DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text,
+  `end_date` datetime NOT NULL,
+  `end_time` varchar(255) DEFAULT NULL,
+  `entrance_price` float DEFAULT NULL,
+  `show_author` bit(1) NOT NULL,
+  `start_date` datetime NOT NULL,
+  `start_time` varchar(255) DEFAULT NULL,
+  `stockitem_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_mt8ulcc4k7fnc56rxaeu1sa33` (`name`),
+  KEY `FK_9vomkx3y0w408eqkaptii98e9` (`stockitem_id`),
+  CONSTRAINT `FK_9vomkx3y0w408eqkaptii98e9` FOREIGN KEY (`stockitem_id`) REFERENCES `stock_item` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `event`
+--
+
+LOCK TABLES `event` WRITE;
+/*!40000 ALTER TABLE `event` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_line`
+--
+
+DROP TABLE IF EXISTS `order_line`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_line` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date_created` datetime DEFAULT NULL,
+  `is_second_hand` bit(1) NOT NULL,
+  `postage` decimal(19,2) DEFAULT NULL,
+  `quantity` int(11) NOT NULL,
+  `sell_price` decimal(19,2) NOT NULL,
+  `web_reference` varchar(255) NOT NULL,
+  `customer_id` bigint(20) NOT NULL,
+  `stock_item` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_3o5umo9vljpmas3s0gipm19m6` (`customer_id`),
+  KEY `FK_1ulg0qibm67hamjfx0sm430rd` (`stock_item`),
+  CONSTRAINT `FK_1ulg0qibm67hamjfx0sm430rd` FOREIGN KEY (`stock_item`) REFERENCES `stock_item` (`id`),
+  CONSTRAINT `FK_3o5umo9vljpmas3s0gipm19m6` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_line`
+--
+
+LOCK TABLES `order_line` WRITE;
+/*!40000 ALTER TABLE `order_line` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_line` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reading_list`
+--
+
+DROP TABLE IF EXISTS `reading_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reading_list` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date_created` datetime DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `is_on_sidebar` bit(1) DEFAULT NULL,
+  `is_on_website` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_fwsvxydyl5u3x3r2qv1owp1k3` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reading_list`
+--
+
+LOCK TABLES `reading_list` WRITE;
+/*!40000 ALTER TABLE `reading_list` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reading_list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reading_list_stock_item`
+--
+
+DROP TABLE IF EXISTS `reading_list_stock_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reading_list_stock_item` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date_created` datetime DEFAULT NULL,
+  `position` bigint(20) DEFAULT NULL,
+  `readinglist_id` bigint(20) DEFAULT NULL,
+  `stockitem_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_6v5o4xqyfnmvvig74m5ajud30` (`readinglist_id`),
+  KEY `FK_8wunn4eyqjd4ijbn70fab3a24` (`stockitem_id`),
+  CONSTRAINT `FK_8wunn4eyqjd4ijbn70fab3a24` FOREIGN KEY (`stockitem_id`) REFERENCES `stock_item` (`id`),
+  CONSTRAINT `FK_6v5o4xqyfnmvvig74m5ajud30` FOREIGN KEY (`readinglist_id`) REFERENCES `reading_list` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reading_list_stock_item`
+--
+
+LOCK TABLES `reading_list_stock_item` WRITE;
+/*!40000 ALTER TABLE `reading_list_stock_item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reading_list_stock_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stock_item`
+--
+
+DROP TABLE IF EXISTS `stock_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stock_item` (
+  `id` bigint(20) NOT NULL,
+  `always_in_stock` bit(1) DEFAULT NULL,
+  `availability` varchar(255) NOT NULL,
+  `binding` varchar(255) NOT NULL,
+  `bouncy_idx` bigint(20) DEFAULT NULL,
+  `category_name` varchar(255) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `depth` bigint(20) DEFAULT NULL,
+  `dimensions` varchar(255) DEFAULT NULL,
+  `gardners_stock_level` bigint(20) DEFAULT NULL,
+  `height` bigint(20) DEFAULT NULL,
+  `img_filename` varchar(255) DEFAULT NULL,
+  `img_url` varchar(255) DEFAULT NULL,
+  `available_at_suppliers` bit(1) DEFAULT NULL,
+  `isbn` varchar(255) NOT NULL,
+  `isbn_as_number` bigint(20) NOT NULL,
+  `merchandise_idx` bigint(20) DEFAULT NULL,
+  `no_of_pages` int(11) DEFAULT NULL,
+  `parent_category_id` bigint(20) DEFAULT NULL,
+  `postage` decimal(19,2) NOT NULL,
+  `price_at_az` decimal(19,2) DEFAULT NULL,
+  `price_third_party_collectable` decimal(19,2) DEFAULT NULL,
+  `price_third_party_new` decimal(19,2) DEFAULT NULL,
+  `price_third_party_second_hand` decimal(19,2) DEFAULT NULL,
+  `published_date` datetime DEFAULT NULL,
+  `publisher_id` bigint(20) DEFAULT NULL,
+  `publisher_name` varchar(255) DEFAULT NULL,
+  `publisher_price` decimal(19,2) NOT NULL,
+  `quantity_in_stock` bigint(20) NOT NULL,
+  `review_as_html` text,
+  `review_as_text` text,
+  `review_short` varchar(255) DEFAULT NULL,
+  `sales_last_year` int(11) NOT NULL,
+  `sales_total` int(11) NOT NULL,
+  `sell_price` decimal(19,2) NOT NULL,
+  `sticky_category_idx` bigint(20) DEFAULT NULL,
+  `sticky_type_idx` bigint(20) DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `width` bigint(20) DEFAULT NULL,
+  `category_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_9cw8tbx3leu3s5qtt183n1fh3` (`isbn`),
+  UNIQUE KEY `UK_fnayeqhxmjoq9wlgaapwob5ii` (`isbn_as_number`),
+  KEY `FK_d5yvxlbyikal47vicsume7esj` (`category_id`),
+  CONSTRAINT `FK_d5yvxlbyikal47vicsume7esj` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stock_item`
+--
+
+LOCK TABLES `stock_item` WRITE;
+/*!40000 ALTER TABLE `stock_item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stock_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stock_item_authors`
+--
+
+DROP TABLE IF EXISTS `stock_item_authors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stock_item_authors` (
+  `stockitem_id` bigint(20) NOT NULL,
+  `author_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`stockitem_id`,`author_id`),
+  KEY `FK_emom8hxq3txd9muvsj1ncqlod` (`author_id`),
+  CONSTRAINT `FK_my4ug7uuxjnmrjmb3rl4mvfk2` FOREIGN KEY (`stockitem_id`) REFERENCES `stock_item` (`id`),
+  CONSTRAINT `FK_emom8hxq3txd9muvsj1ncqlod` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stock_item_authors`
+--
+
+LOCK TABLES `stock_item_authors` WRITE;
+/*!40000 ALTER TABLE `stock_item_authors` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stock_item_authors` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-04-07 11:57:52
---
+++ src/main/scripts/sql/populate_db.sql
@@ -0,0 +1,193 @@
+-- Politics category
+-- update bookmarks.stockitem set category_id = 20 where category_id = 108 and title like '%music%';
+
+-- update bookmarks.stockitem set category_id = 45 where category_id = 108 and title like '%philosophy%';
+
+-- update bookmarks.stockitem set category_id = 4 where category_id = 108 and title like '%africa%';
+-- update bookmarks.stockitem set category_id = 4 where category_id = 108 and title like '%nigeria%';
+-- update bookmarks.stockitem set category_id = 4 where category_id = 108 and title like '%ghana%';
+
+-- update bookmarks.stockitem set category_id = 28 where category_id = 108 and title like '%feminis%';
+
+-- update bookmarks.stockitem set category_id = 30 where category_id = 108 and title like '%europe%';
+-- update bookmarks.stockitem set category_id = 30 where category_id = 108 and title like '%france%';
+-- update bookmarks.stockitem set category_id = 30 where category_id = 108 and title like '%germany%';
+-- update bookmarks.stockitem set category_id = 30 where category_id = 108 and title like '%portugal%';
+-- update bookmarks.stockitem set category_id = 30 where category_id = 108 and title like '%spain%';
+-- update bookmarks.stockitem set category_id = 30 where category_id = 108 and title like '%poland%';
+-- update bookmarks.stockitem set category_id = 30 where category_id = 108 and title like '%belgium%';
+
+-- update bookmarks.stockitem set category_id = 5 where category_id = 108 and title like '%china%';
+-- update bookmarks.stockitem set category_id = 5 where category_id = 108 and title like '%india%';
+-- update bookmarks.stockitem set category_id = 5 where category_id = 108 and title like '%japan%';
+-- update bookmarks.stockitem set category_id = 5 where category_id = 108 and title like '%thailand%';
+-- update bookmarks.stockitem set category_id = 5 where category_id = 108 and title like '%asia%';
+-- update bookmarks.stockitem set azPublisher_id = 725 where publisher_id = 305;
+-- update bookmarks.stockitem set azPublisher_id = 725 where azPublisher_id in (96,730);
+update bookmarks.stockitem set img_url = null where img_url = 'http://g-ecx.images-amazon.com/images/G/02/misc/no-img-lg-uk.gif';
+
+-- Mugs all have postage of 2.25 (2.15 + 1.75base = 4), and 8 pounds on website
+update bookmarks.stockitem set postage = 2.15 where stockitemtype = 'MUG';
+
+-- Zed Books Ltd
+-- update bookmarks.stockitem set azPublisher_id = 145 where azPublisher_id in (select id from bookmarks.az_publisher where name like 'zed %');
+
+-- categories
+insert into bmw.category (id, name, is_on_website, is_in_sidebar, parent_id) select id, name, is_on_website, is_in_sidebar, parent_id from bookmarks.category c where c.parent_id is null;
+insert into bmw.category (id, name, is_on_website, is_in_sidebar, parent_id) select id, name, is_on_website, is_in_sidebar, parent_id from bookmarks.category c where c.parent_id is not null;
+
+-- authors
+insert into bmw.author (id, name) select id, name from bookmarks.author;
+
+-- Update postage to default if null or 0
+update bookmarks.stockitem set postage = 1.05 where postage is null or postage = 0;
+
+-- transfer stock items;
+insert into bmw.stock_item (
+ id,
+ availability,
+ binding,
+ isbn,
+ isbn_as_number,
+ published_date,
+ publisher_price,
+ quantity_in_stock,
+ sell_price,
+ type,
+ title,
+ postage,
+ dimensions,
+ no_of_pages,
+ review_as_text,
+ review_as_html,
+ img_url,
+ img_filename,
+ publisher_id,
+ price_at_az,
+ price_third_party_collectable,
+ price_third_party_new,
+ price_third_party_second_hand,
+ sticky_category_idx,
+ sticky_type_idx,
+ bouncy_idx,
+ category_id,
+ gardners_stock_level,
+ always_in_stock,
+ available_at_suppliers
+ )
+select 
+id,
+availability,
+binding,
+isbn,
+isbnAsNumber, 
+publishedDate,
+publisherPrice,
+quantityInStock,
+sellPrice,
+stockitemtype,
+title,
+postage,
+dimensions,
+noOfPages,
+review_as_text,
+review_as_html,
+img_url,
+img_filename,
+publisher_id,
+price_at_az,
+price_third_party_collectable,
+price_third_party_new,
+price_third_party_second_hand,
+sticky_category_idx,
+sticky_type_idx,
+bouncy_idx,
+category_id,
+gardners_stock_level,
+always_in_stock,
+available_at_suppliers
+from bookmarks.stockitem si where si.put_on_website = true;
+
+-- Update Parent category
+update bmw.stock_item si, bmw.category c set si.parent_category_id = c.parent_id where c.id = si.category_id;
+
+-- fill in parent category 
+update bmw.stock_item si set si.parent_category_id = si.category_id where si.parent_category_id is null;
+
+-- Get short review
+update bmw.stock_item set review_short = substring(review_as_text, 1, 190);
+update bmw.stock_item set review_short = null where review_short = '';
+
+-- Copy over publisher name
+update bookmarks.stockitem si, bmw.stock_item bsi set bsi.publisher_id = si.publisher_id where bsi.id = si.id;
+update bmw.stock_item si, bookmarks.publisher p set si.publisher_name = p.name where p.id = si.publisher_id; 
+
+-- Copy over category name
+update bmw.stock_item si, bookmarks.category c set si.category_name = c.name where c.id = si.category_id; 
+
+-- Is the review to be shown
+update bmw.stock_item si, bookmarks.stockitem bsi set si.review_as_text = null, si.review_short = null where bsi.put_review_on_website = false and bsi.id = si.id;
+
+-- Sales last year
+update bmw.stock_item si set sales_last_year = (select sum(s.quantity) from bookmarks.sale s where stockItem_id = si.id and s.creationDate >= DATE_SUB(NOW(),INTERVAL 1 YEAR));
+update bmw.stock_item si set sales_total = (select sum(s.quantity) from bookmarks.sale s where stockItem_id = si.id);
+
+-- Images, has stock item got an image?
+update  bmw.stock_item si, bookmarks.stockitem bsi set si.img_url = null where bsi.put_image_on_website = false and bsi.id = si.id;
+update bmw.stock_item set img_url = null where img_url = 'http://g-ecx.images-amazon.com/images/G/02/misc/no-img-lg-uk.gif';
+update bmw.stock_item set img_url = null where img_url = '';
+
+-- Copy over stockitem authors
+insert into bmw.stock_item_authors (stockitem_id, author_id) (select stockitem_id, author_id from bookmarks.stockitem_author sia join bmw.stock_item si on si.id = sia.stockitem_id);
+
+-- Copy over stockitem categories
+-- insert into bmw.stock_item_categories (stockitem_id, category_id) (select si.id, category_id from bookmarks.stockitem si join bmw.stock_item bmwsi on si.id = bmwsi.id);
+
+-- Sort out price for 2nd hand
+update bmw.stock_item si set sell_price = price_third_party_second_hand + 8 + RAND() where si.availability = 'OUT_OF_PRINT';
+
+-- Sort out price for 3rd Party new
+update bmw.stock_item si set sell_price = price_third_party_new + 8 + RAND() where si.availability = 'AVAILABLE_NEW_FROM_THIRD_PARTY';
+
+-- Deazify
+update bmw.stock_item set review_short = null where review_short like '%Read a customer review or write one%';
+update bmw.stock_item set review_short = null where review_short like '%amazon%';
+update bmw.stock_item set review_as_text = null, review_as_html = null where review_as_text like '%amazon%';
+update bmw.stock_item set review_as_text = null, review_as_html = null where review_as_text like '%Read a customer review or write one%';
+
+-- Price changes on website
+update bmw.stock_item set sell_price = 8 where type = 'MUG';
+
+-- READING LIST
+-- Only copy across reading lists on website
+INSERT INTO bmw.reading_list (id, name, is_on_website, is_on_sidebar) select id, name, is_on_website, is_on_sidebar from bookmarks.reading_list brl;
+
+INSERT INTO bmw.reading_list_stock_item (stockitem_id, readinglist_id, position) 
+	select rlsi.stockItems_id, rlsi.reading_list_id, rlsi.stockItem_idx from bookmarks.reading_list_stockitem rlsi
+	join bookmarks.reading_list brl  on brl.id = rlsi.reading_list_id;
+
+-- Events
+update bookmarks.event set description = replace(description, '<br/>', '');
+update bookmarks.event set description = replace(description, '<br />', '');
+update bookmarks.event set description = replace(description, '<p>&nbsp;</p>', '');
+
+insert into bmw.event (id, name, start_date, end_date, description, start_time, end_time, stockitem_id, show_author, entrance_price)
+	select id, name, startDate, endDate, description, startTime, endTime, stockItem_id, show_author, entrance_price from bookmarks.event where onWebsite = true and startDate > now();
+
+-- Escape apostophes
+update bmw.stock_item set review_as_html = replace(review_as_html, '’', '&#39;');
+
+-- Indexes
+create index type_idx on bmw.stock_item (type);
+create index parent_category_idx on bmw.stock_item (parent_category_id);
+create index sales_last_year_idx on bmw.stock_item (sales_last_year);
+create index quantity_in_stock_idx on bmw.stock_item (quantity_in_stock);
+create index published_date_idx on bmw.stock_item (published_date);
+create index publisher_id_idx on bmw.stock_item (publisher_id);
+create index sticky_category_idx on bmw.stock_item (sticky_category_idx);
+create index bouncy_idx on bmw.stock_item (bouncy_idx);
+create index sticky_type_idx on bmw.stock_item (sticky_type_idx);
+
+
+
+
