e9aba1b4d8ef8b6736469188048684d4f6906bd9
jbeaken <jack747@gmail.com>
2015-05-26 14:15:32 +0100

    Removing csv dependecies from pom

--- src/main/java/org/bookmarks/service/AdminServiceImpl.java
+++ src/main/java/org/bookmarks/service/AdminServiceImpl.java
@@ -14,21 +14,21 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 import org.bookmarks.domain.Customer;
 
-import au.com.bytecode.opencsv.CSVReader;
+//import au.com.bytecode.opencsv.CSVReader;
 
 
 @Service
 public class AdminServiceImpl implements AdminService {
-	
+
 	@Autowired
 	private CustomerService customerService;
-	
+
 	/**
 	 * Load account details from CSV
 	 * Check if credit notes exist, if not create, if so update
 	 * Read in current balance and opening balance
-	 * 
-	 * 
+	 *
+	 *
 "Customers: Kirkwood, Richard J",,,,,,,,
 37607,,,,,,,,
 01/09/2011,,,,Balance brought forward,,,,0
@@ -46,12 +46,12 @@ public class AdminServiceImpl implements AdminService {
 
 30/06/2012,,,,Balance carried forward,,,,-350.92
 	 */
-	
+
 	@Override
 	public List<String> uploadAccounts() throws IOException {
 		List<String> messages = new ArrayList<String>();
 		List<Customer> customerWithAccount= new ArrayList<Customer>();
-		
+
 		//Populate list with customers and their account details
 		List<String> lines = FileUtils.readLines(new File("c:\\accounts.csv"));
 		Integer index = 0;
@@ -66,7 +66,7 @@ public class AdminServiceImpl implements AdminService {
 			index++;
 		}
 
-		
+
 		//Update entities
 		for(Customer customer : customerWithAccount) {
 			//Get customer from database
@@ -78,8 +78,8 @@ public class AdminServiceImpl implements AdminService {
 			}
 			if(dbCustomer.getBookmarksAccount().getAccountHolder() == null || dbCustomer.getBookmarksAccount().getAccountHolder() == false) {
 				messages.add(customer.getFullName() +  " isn't account holder");
-			} 
-			
+			}
+
 			//Update
 			if(!customer.getFirstName().equals(dbCustomer.getFirstName()) || !customer.getLastName().equals(dbCustomer.getLastName())) {
 				messages.add("Names don't match: " + customer.getFullName() + " : " + dbCustomer.getFullName() + " ID:" + customer.getId());
@@ -89,7 +89,7 @@ public class AdminServiceImpl implements AdminService {
 		return messages;
 	}
 
-	private Customer getCustomerAccountInfo(List<String> lines, Integer index, List<String> messages) {		
+	private Customer getCustomerAccountInfo(List<String> lines, Integer index, List<String> messages) {
 		Customer customer = getCustomerName(lines, index);
 		System.out.print(customer.getFullName());
 
@@ -105,10 +105,10 @@ public class AdminServiceImpl implements AdminService {
 			return null;
 		}
 		System.out.print(" " + customer.getId());
-		
+
 		//Next line is not useful (yet)
 		index++;
-		
+
 		//Next line is opening balance
 		index++;
 		BigDecimal openingBalance = getOpeningBalance(customer.getFullName(), lines, index);
@@ -116,7 +116,7 @@ public class AdminServiceImpl implements AdminService {
 			//Try next line, wierd
 			index++;
 			openingBalance = getOpeningBalance(customer.getFullName(), lines, index);
-			if(openingBalance == null) { 
+			if(openingBalance == null) {
 				//Didn't work, reset
 //				openingBalance = new BigDecimal(0);
 				index--;
@@ -124,10 +124,10 @@ public class AdminServiceImpl implements AdminService {
 		}
 		openingBalance = openingBalance.multiply(new BigDecimal(-1));
 		customer.getBookmarksAccount().setOpeningBalance(openingBalance);
-		
+
 		//Next lines are credits/debits, do later
 		setCurrentBalance(customer, lines, index);
-		
+
 		return customer;
 	}
 
@@ -170,13 +170,13 @@ public class AdminServiceImpl implements AdminService {
 		try {
 			int openingBalanceIndex = openingBalanceLine.indexOf("Opening balances") + 23;
 			openingBalanceLine = openingBalanceLine.substring(openingBalanceIndex);
-			openingBalanceIndex = openingBalanceLine.indexOf("\"");		
+			openingBalanceIndex = openingBalanceLine.indexOf("\"");
 			openingBalanceLine = openingBalanceLine.substring(openingBalanceIndex);
 			int lastCommaIndex = openingBalanceLine.lastIndexOf(",");
 			String openingBalanceString = openingBalanceLine.substring(lastCommaIndex + 1, openingBalanceLine.length());
 			try {
 				System.out.print(" openingBalanceString = " + openingBalanceString + " ");
-				//Strange, some are enclosed by quotes. 
+				//Strange, some are enclosed by quotes.
 				int quoteIndex = openingBalanceString.indexOf("\"");
 				if(quoteIndex != -1) {
 					openingBalanceString = openingBalanceString.substring(0, openingBalanceString.length() - 1);
