2e83cc50197e66f5a4d628942bc0171a63de002d
jbeaken <jack747@gmail.com>
2015-06-11 13:41:50 +0100

    Rejigging build.sh
    Former-commit-id: a661f62af11042817acd1af754b7f8322a233753

--- src/main/scripts/build.sh
+++ src/main/scripts/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Script to build beans from git
-git pull
+#git pull
+systemctl stop tomcat
 /opt/maven/bin/mvn clean install
 cp /opt/tomcat/webapps/bookmarks.war /home/bak/
 #rm -rf /opt/tomcat/webapps/bookmarks/
 #rm /opt/tomcat/logs/*
 cp target/bookmarks.war /opt/tomcat/webapps/bookmarks.war
-restart tomcat
+systemctl start tomcat
