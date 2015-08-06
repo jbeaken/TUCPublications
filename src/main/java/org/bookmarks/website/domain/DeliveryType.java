8f36647658170450d306d872f420d132b53c28ef
jbeaken <jack747@gmail.com>
2015-08-06 14:18:20 +0100

    Adding sponsorship delivery type
    Former-commit-id: 315c56fd1ba99efbb42a1ce2ac67805c841a1b18

--- src/main/java/org/bookmarks/website/domain/DeliveryType.java
+++ src/main/java/org/bookmarks/website/domain/DeliveryType.java
@@ -1,15 +1,15 @@
 package org.bookmarks.website.domain;
 
 public enum DeliveryType {
-	MAIL("Mail Order"), COLLECTION("Collection");
-	
+	MAIL("Mail Order"), COLLECTION("Collection"), SPONSORSHIP("Sponsorship");
+
 	private final String displayName;
 
-    private DeliveryType(String displayName) {
-        this.displayName = displayName;
-    }
+		private DeliveryType(String displayName) {
+				this.displayName = displayName;
+		}
 
-    public String getDisplayName() {
-        return displayName;
-    }
+		public String getDisplayName() {
+				return displayName;
+		}
 }
