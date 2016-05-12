96a4d9b9b28bff0b33b46402aad47aa83209c8ef
jbeaken <jack747@gmail.com>
2016-05-12 18:50:31 +0100

    Upgrading install scripts
    Former-commit-id: e9a4e8be4c27b1847080005086759339124b20cc

--- src/main/scripts/install.sh
+++ src/main/scripts/install.sh
@@ -1,3 +1,10 @@
+# Install git
+apt-get -y install git
+mkdir /home/git
+cd /home/git
+git clone ssh://git@109.109.239.50:2298/home/git/bookmarks bookmarks
+
+# Install Java
 # Accept license automatically
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
 
@@ -6,29 +13,29 @@ echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /e
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 apt-get update
-apt-get install oracle-java8-installer
+apt-get -y install oracle-java8-installer
 #apt-get install oracle-java8-set-default
 
 # Git (requires password entry)
-apt-get install git
+apt-get -y install git
 mkdir /home/git
 git clone ssh://git@109.109.239.50:2298/home/git/bookmarks /home/git/bookmarks
 
 # Tomcat
-wget -P /opt http://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-8/v8.0.23/bin/apache-tomcat-8.0.23.tar.gz
-tar  -xzC /opt -f /opt/apache-tomcat-8.0.23.tar.gz
-ln -s /opt/apache-tomcat-8.0.23 /opt/tomcat
+wget -P /opt http://www.mirror.vorboss.net/apache/tomcat/tomcat-8/v8.0.33/bin/apache-tomcat-8.0.33.tar.gz
+tar  -xzC /opt -f /opt/apache-tomcat-8.0.33.tar.gz
+ln -s /opt/apache-tomcat-8.0.33 /opt/tomcat
 rm -rf /opt/tomcat/webapps/*
 cp /home/git/bookmarks/src/main/scripts/server.xml /opt/tomcat/conf/server.xml
 cp /home/git/bookmarks/src/main/scripts/setenv.sh /opt/tomcat/bin/setenv.sh
 
 # Maven
-wget -P /opt ftp://mirror.reverse.net/pub/apache/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz
-tar -xzC /opt -f /opt/apache-maven-3.3.3-bin.tar.gz
-ln -s /opt/apache-maven-3.3.3 /opt/maven
+wget -P /opt ftp://mirror.reverse.net/pub/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+tar -xzC /opt -f /opt/apache-maven-3.3.9-bin.tar.gz
+ln -s /opt/apache-maven-3.3.9 /opt/maven
 
 # Mysql
-apt-get install mysql-server
+apt-get -y install mysql-server
 
 # Download bookmarks database.
 mkdir -p /home/bookmarks/logs
