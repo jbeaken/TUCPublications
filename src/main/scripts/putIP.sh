bb733c2e770f3bbf675a898f2b4f1b386914d9a5
jbeaken <jack747@gmail.com>
2016-08-18 12:56:41 +0100

    Removing validate from hdml
    Former-commit-id: c08af8e1e92f94e9d9889731fcc78dfa6f8d4090

---
+++ src/main/scripts/putIP.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+curl http://bot.whatismyipaddress.com > ip.txt
+#wput ip.txt ftp://u34059913-kevin:obama08elected@217.160.101.49/backup/ip.txt
+wput -nc -u ip.txt ftp://u73194415-7wandX3:EDE7%25pvy3H@s468164439.websitehome.co.uk/bookmarks/ip.txt
+
+
