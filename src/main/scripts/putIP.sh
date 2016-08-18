b24bdabe69c5714abd2d59e5f27cbbd18441a5eb
jbeaken <jack747@gmail.com>
2016-08-18 12:58:17 +0100

    Adding putIP.sh
    Former-commit-id: dd839c41277b5a779011a551bee1b35a84823f88

--- src/main/scripts/putIP.sh
+++ src/main/scripts/putIP.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+
+#Load password
+. ./password
+
+# Get ip address
 curl http://bot.whatismyipaddress.com > ip.txt
-#wput ip.txt ftp://u34059913-kevin:obama08elected@217.160.101.49/backup/ip.txt
-wput -nc -u ip.txt ftp://u73194415-7wandX3:EDE7%25pvy3H@s468164439.websitehome.co.uk/bookmarks/ip.txt
+
+# Put on ftp server
+wput -nc -u ip.txt ftp://u73194415-7wandX3:$PASSWORD@s468164439.websitehome.co.uk/bookmarks/ip.txt
 
 
