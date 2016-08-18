05de9cee8365a8d51ee1c732590e824627145085
jbeaken <jack747@gmail.com>
2016-08-18 13:31:18 +0100

    Debug to putIP.sh
    Former-commit-id: 38f0c4a6e02b0fb09fa634d5907d0b240524cd50

--- src/main/scripts/putIP.sh
+++ src/main/scripts/putIP.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-
-#Load password
-. ./password
-
-# Get ip address
 curl http://bot.whatismyipaddress.com > ip.txt
 
-# Put on ftp server
+echo "Placing ip address on ftp servers"
+
 wput -nc -u ip.txt ftp://u73194415-7wandX3:$PASSWORD@s468164439.websitehome.co.uk/bookmarks/ip.txt
 
 
