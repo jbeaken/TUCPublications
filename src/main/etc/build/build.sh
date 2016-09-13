260f0354f89a0c4a3bd9099a2f4d9c5b4408931e
jbeaken <jack747@gmail.com>
2016-09-13 22:46:27 +0100

    Improving build script

--- src/main/etc/build/build.sh
+++ src/main/etc/build/build.sh
@@ -2,29 +2,34 @@
 # Script to build beans from git
 
 # Make sure only root can run our script
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
+if [ "$(id -u)" = "0" ]; then
+   echo "This script must not be run as root" 1>&2
    exit 1
 fi
 
 # Get latest from repository
 git pull
 
-. /etc/environment
+if [ "$1" = "1" ]; then 
+	exit 1
+fi
 
 # Clean and build
 mvn clean package
+if [ "$1" = "1" ]; then
+        exit 1
+fi
 
 # Back up
 cp /usr/local/share/tomcat/webapps/bookmarks.war /home/bak/
 
 # Stop tomcat
-systemctl stop tomcat
+sudo systemctl stop tomcat
 
-rm -rf /usr/local/share/tomcat/webapps/bookmarks*
-rm /usr/local/share/tomcat/logs/*
+sudo rm -rf /usr/local/share/tomcat/webapps/bookmarks*
+sudo rm /usr/local/share/tomcat/logs/*
 
-cp target/bookmarks.war /usr/local/share/tomcat/webapps/bookmarks.war
+sudo cp target/bookmarks.war /usr/local/share/tomcat/webapps/bookmarks.war
 
 # Start tomcat
-systemctl start tomcat
+sudo systemctl start tomcat
