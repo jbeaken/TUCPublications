c016fcd2dacba31d19f377d352035774b521b983
jbeaken <jack747@gmail.com>
2017-01-10 13:47:50 +0000

    Adding csv import for accounts
    Former-commit-id: 780c5175bd95c9dd9ed8255f6843df60ca3cda46

--- src/main/groovy/org/bookmarks/groovy/ImportBookmarksAccountCreditsFromCSV.groovy
+++ src/main/groovy/org/bookmarks/groovy/ImportBookmarksAccountCreditsFromCSV.groovy
@@ -1,13 +1,13 @@
-// From VT :
-// Display Ledger Balances, customers, copy entire report button,
-// Past into libre calc, save as csv, edit filters, delimiter tab
-/// copy to  /home/bookmarks/accounts.csv
-// Run this script
-
 package org.bookmarks.groovy
+
+@Grapes(
+    @Grab(group='com.opencsv', module='opencsv', version='3.8')
+)
 
 import groovy.sql.Sql;
 import groovy.grape.Grape;
+import com.opencsv.CSVReader
+
 
 //Load the mysql driver
 Grape.grab(group:'mysql', module:'mysql-connector-java', version:'5.1.39', classLoader: this.class.classLoader.rootLoader)
@@ -15,23 +15,14 @@ this.class.classLoader.getURLs().each{
   ClassLoader.systemClassLoader.addURL(it);
 }
 
-def sql = Sql.newInstance("jdbc:mysql://localhost:3306/bookmarks", "root", "cyclops", "com.mysql.jdbc.Driver")
+def sql = Sql.newInstance("jdbc:mysql://localhost:3306/bookmarks", "root", "admin", "com.mysql.jdbc.Driver")
 
 count = 0
 
-new File("/home/bookmarks/bank.csv").splitEachLine(",") {fields ->
+CSVReader reader = new CSVReader(new FileReader("/home/bookmarks/bank.csv"));
 
-//	def customerName = fields[0]
-  def field1 = fields[0]      //This will be customer id, of blank
-	def field2 = fields[1]      //This will be customer id, of blank
-	def field3 = fields[2]
-	def field4 = fields[3]
-	def field5 = fields[4]
-  def field6 = fields[5]
+println reader
 
 
-println field1
-    println field5
-} //end splitEachLine
-//sql.executeUpdate('update customer set paysInMonthly = true where amountPaidInMonthly > 0')
+
 println "Finished - Updated ${count} accounts"
