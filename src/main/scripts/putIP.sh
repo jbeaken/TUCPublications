6d35cfaa50fb864ba1c9962ba445cf543a7b6bc0
jbeaken <jack747@gmail.com>
2017-02-16 18:51:13 +0000

    Closing session

--- src/main/scripts/putIP.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-curl http://bot.whatismyipaddress.com > ip.txt
-
-echo "Placing ip address on ftp servers"
-
-wput -nc -u ip.txt ftp://u73194415-7wandX3:$PASSWORD@s468164439.websitehome.co.uk/bookmarks/ip.txt
-
-
