c2c92f55d42db6c2b1f50438318f57e9bc9a1ecc
jbeaken <jack747@gmail.com>
2017-05-25 11:37:41 +0100

    Removed pound symbols from from csv export which were corrupted in libercalc

--- src/main/java/org/bookmarks/ui/AbstractBookmarksTableDecorator.java
+++ src/main/java/org/bookmarks/ui/AbstractBookmarksTableDecorator.java
@@ -21,30 +21,32 @@ import org.displaytag.model.TableModel;
 import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.number.CurrencyFormatter;
 import org.springframework.format.number.CurrencyStyleFormatter;
+import org.springframework.format.number.NumberStyleFormatter;
 
 public abstract class AbstractBookmarksTableDecorator extends TableDecorator {
-	
+
 //	protected DateFormat dateFormat = new SimpleDateFormat("dd/MMM/yyyy");
 	protected DateFormatter dateFormatter = new DateFormatter("dd/MMM/yyyy");
 	protected DateFormatter shortDateFormatter = new DateFormatter("dd/MM/yy");
 	protected DateFormatter dateTimeFormatter = new DateFormatter("dd/MM/yy HH:mm");
+	protected NumberStyleFormatter numberStyleFormatter = new NumberStyleFormatter("###.##");
 	protected CurrencyStyleFormatter currencyFormatter = new CurrencyStyleFormatter();
-	protected String contextPath; 
-	
+	protected String contextPath;
+
 	public void init(PageContext pageContext, Object decorated, TableModel tableModel) {
          super.init(pageContext, decorated, tableModel);
          contextPath = getPageContext().getServletContext().getContextPath();
     }
-    
+
 	protected String getImage(String src, String title) {
 		return "<img src=\"../resources/images/" + src + "\" title=\"" + title + "\" />";
 	}
-	
+
 	public String getCreationDate() {
 		AbstractEntity e = (AbstractEntity)getCurrentRowObject();
 		return dateFormatter.print(e.getCreationDate(), Locale.UK);
 	}
-	
+
 	protected String getStockItemTitleLink(StockItem stockItem, String flow) {
 		return getAnchor(contextPath + "/stock/edit?id=" + stockItem.getId() +"&flow=" + flow,
 				stockItem.getTitle() + "-" + stockItem.getMainAuthor(),"Title", true, false);
@@ -53,29 +55,29 @@ public abstract class AbstractBookmarksTableDecorator extends TableDecorator {
 	protected String getStockItemTitleLinkNoAuthor(StockItem stockItem, String flow) {
 		return getAnchor("/bookmarks/stock/edit?id=" + stockItem.getId() +"&flow=" + flow,
 				stockItem.getTitle(),"Title", true, false);
-	}	
-	
+	}
+
 	protected String getStockTitleLink(StockItem stockItem, String flow) {
 		return getAnchor("/bookmarks/stock/edit?id=" + stockItem.getId() +"&flow=" + flow,
 				stockItem.getTitle(),"Title", true, false);
 	}
-	
+
 //	protected String getImageAnchor(String aUrl, String imgUrl, String title) {
 //		return "<a href=\"" + aUrl + "\">" + getImage(imgUrl, title) + "</a>";
 //	}
-	
+
 	protected String getImageAnchor(String aUrl, String imgUrl, String title, boolean openInParent) {
 		return getImageAnchor(aUrl, imgUrl, title, openInParent, false);
-	}	
-	
+	}
+
 	protected String showSell(StockItem stockItem) {
 		return getImageAnchor("../sale/sellByISBN?isbn=" + stockItem.getIsbn(), "pound_medium.png", "Sell", false);
-	}	
-	
+	}
+
 	protected String confirmation(String url, String imgUrl) {
 		return "<img src='../resources/images/" + imgUrl + "' onclick='javascript:authoriseUser(\"" + url + "\")' />";
 	}
-	
+
 	protected String getImageAnchor(String aUrl, String imgUrl, String title, boolean openInParent, boolean isDisabled) {
 		StringBuffer buffer = null;
 		if(isDisabled) {
@@ -92,8 +94,8 @@ public abstract class AbstractBookmarksTableDecorator extends TableDecorator {
 			buffer.append("</a>");
 		}
 		return buffer.toString();
-	}	
-	
+	}
+
 	protected String getAnchor(String aUrl, String text, String title, boolean openInParent, boolean isDisabled) {
 		StringBuffer buffer = null;
 		if(isDisabled) {
@@ -110,88 +112,88 @@ public abstract class AbstractBookmarksTableDecorator extends TableDecorator {
 			buffer.append("</a>");
 		}
 		return buffer.toString();
-	}	
-	
+	}
+
 	protected String getImage(String src) {
 		return "<img src=\"../resources/images/" + src + "\" title=\"title\" />";
-	}	
-	
+	}
+
 	protected  String showEditNote(AbstractEntity e) {
 		if(e.getNote() != null) return showEditNote(e, "note_available.jpg"); //Change colour for a live note
-		return showEditNote(e, "note.png");	
+		return showEditNote(e, "note.png");
 	}
-	
+
 	protected  String showEditNote(AbstractEntity e, String imageFileName) {
 		long id = e.getId();
 		String note = e.getNote();
 		if(note == null || note.trim().isEmpty()) note = "Add note";
-		return getImageAnchor("displayEditNote?id=" + id, 
-				imageFileName, 
-				note, true, false);		
+		return getImageAnchor("displayEditNote?id=" + id,
+				imageFileName,
+				note, true, false);
 	}
-	
+
 	protected String showEdit(AbstractEntity e) {
 		return showEdit(e, false);
 	}
-	
+
 
 	protected String showEdit(AbstractEntity e,	boolean isDisabled) {
 		return showEdit(e, isDisabled, "write_medium.png");
-	}	
-	
+	}
+
 	protected String showEdit(AbstractEntity e,	boolean isDisabled, String imageFileName) {
-		return getImageAnchor("edit?id=" + e.getId() + "&flow=search", 
-				imageFileName, 
+		return getImageAnchor("edit?id=" + e.getId() + "&flow=search",
+				imageFileName,
 				"Edit", false, isDisabled);
-	}	
+	}
 
 	protected  String showDelete(AbstractEntity e) {
 		return showDelete(e, false);
 	}
-	
+
 	protected  String showDelete(AbstractEntity e, boolean isDisabled) {
-		return getImageAnchor("delete?id=" + e.getId(), 
-				"delete_medium.png", 
+		return getImageAnchor("delete?id=" + e.getId(),
+				"delete_medium.png",
 				"Delete", false, isDisabled);
 	}
-	
+
 	protected  String showView(AbstractEntity e) {
-		return 	getImageAnchor("view?id=" + e.getId() + "&flow=search", 
-				"view.png", 
+		return 	getImageAnchor("view?id=" + e.getId() + "&flow=search",
+				"view.png",
 				"View", false, false);
 	}
-	
+
 	protected String getAddress(Address address) {
-		if(address.getAddress1().isEmpty() 
-				&& address.getAddress2().isEmpty() 
-				&& address.getAddress3().isEmpty() 
-				&& address.getCity().isEmpty() 
+		if(address.getAddress1().isEmpty()
+				&& address.getAddress2().isEmpty()
+				&& address.getAddress3().isEmpty()
+				&& address.getCity().isEmpty()
 				&& address.getPostcode().isEmpty()) return "No address supplied";
 		StringBuffer addressBuffer = new StringBuffer(100);
 		addressBuffer.append(address.getAddress1() + ","); //Cannot be null so no check
 		addressBuffer.append(address.getAddress2() == null || address.getAddress2().isEmpty() ? "" : address.getAddress2() + ", ");
 		addressBuffer.append(address.getCity() == null || address.getCity().isEmpty() ? "" : address.getCity());
 		addressBuffer.append(address.getPostcode() == null ? "" : ", " + address.getPostcode());
-		
+
 		return addressBuffer.toString();
-	}	
-	
+	}
+
 	protected String getContactDetails(Customer customer) {
 		StringBuffer buffer = new StringBuffer();
 		buffer.append(getTelephoneNumber(customer.getContactDetails()));
 		buffer.append("/");
 		buffer.append(getEmail(customer.getContactDetails().getEmail()));
 		return buffer.toString();
-	}	
-	
+	}
+
 	protected String getTelephoneNumber(ContactDetails contactDetails) {
 		if(contactDetails ==  null) return "None";
-		if(contactDetails.getHomeNumber() ==  null 
-				&& contactDetails.getMobileNumber() ==  null 
+		if(contactDetails.getHomeNumber() ==  null
+				&& contactDetails.getMobileNumber() ==  null
 				&& contactDetails.getWorkNumber() ==  null ) return "None";
 
 		StringBuffer telephoneBuffer = new StringBuffer(100);
-		
+
 		if(contactDetails.getMobileNumber() !=  null  && !contactDetails.getMobileNumber().isEmpty()) {
 			telephoneBuffer.append(contactDetails.getMobileNumber());
 		}
@@ -203,32 +205,32 @@ public abstract class AbstractBookmarksTableDecorator extends TableDecorator {
 			if(telephoneBuffer.length() != 0) telephoneBuffer.append(", ");
 			telephoneBuffer.append(contactDetails.getWorkNumber());
 		}
-		
+
 		if(telephoneBuffer.length() == 0) {
 			telephoneBuffer.append("None");
 		}
-		
-		return telephoneBuffer.toString();	
-	}	
-	
-	protected String getCustomerName(Customer customer) {
-		return getAnchor("/bookmarks/customer/edit?id=" + customer.getId(), getName(customer), "Edit", true, false);		
+
+		return telephoneBuffer.toString();
 	}
-	
+
+	protected String getCustomerName(Customer customer) {
+		return getAnchor("/bookmarks/customer/edit?id=" + customer.getId(), getName(customer), "Edit", true, false);
+	}
+
 	protected String getCustomerName(Customer customer, String flow) {
-		return getAnchor("/bookmarks/customer/edit?id=" + customer.getId() + "&flow=" + flow, getName(customer), "Edit", true, false);		
-	}	
-	
+		return getAnchor("/bookmarks/customer/edit?id=" + customer.getId() + "&flow=" + flow, getName(customer), "Edit", true, false);
+	}
+
 	public String getName(Customer customer) {
 		return customer.getFirstName() + " " + customer.getLastName();
 	}
-	
+
 	protected String getEmail(String email) {
 		if(email == null ||email.isEmpty()) {
 			return "No email";
 		}
 		return email;
-	}	
+	}
 
 	protected String getContactName(String contactName) {
 		if(contactName == null || contactName.isEmpty()) {
@@ -236,32 +238,31 @@ public abstract class AbstractBookmarksTableDecorator extends TableDecorator {
 		}
 		return contactName;
 	}
-	
+
 	protected String getAddress(Customer customer) {
 		return getAddress(customer.getAddress());
 	}
-	
+
 	protected String getPhoneNumber(Customer customer) {
 		ContactDetails telephoneDirectory = customer.getContactDetails();
-		
+
 		StringBuffer telephoneBuffer = new StringBuffer(100);
 		telephoneBuffer.append(telephoneDirectory.getMobileNumber() + ", ");
 		telephoneBuffer.append("H: " + (telephoneDirectory.getHomeNumber() == null ? "" : telephoneDirectory.getHomeNumber()) + ", ");
 		telephoneBuffer.append("W: " + (telephoneDirectory.getWorkNumber() == null ? "" : telephoneDirectory.getWorkNumber()) + ", ");
-		
+
 		return telephoneBuffer.toString();
 	}
-	
+
 	protected String getBinding() {
 		StockItem stockItem = (StockItem)getCurrentRowObject();
 		if(stockItem.getBinding() == null) return "N/A";
 		switch(stockItem.getBinding()) {
-		case HARDBACK: 
+		case HARDBACK:
 			return "hb";
 		case PAPERBACK:
 			return "pb";
 		}
 		return "N/A";
-	}	
+	}
 }
-
--- src/main/java/org/bookmarks/ui/SearchStockItemsDecorator.java
+++ src/main/java/org/bookmarks/ui/SearchStockItemsDecorator.java
@@ -6,7 +6,7 @@ import org.bookmarks.domain.StockItem;
 
 
 public class SearchStockItemsDecorator extends AbstractBookmarksTableDecorator {
-	
+
 	protected String getInfo(StockItem stockItem) {
 		StringBuilder info = new StringBuilder();
 		if(stockItem.isBook()){
@@ -20,7 +20,7 @@ public class SearchStockItemsDecorator extends AbstractBookmarksTableDecorator {
 				+ stockItem.getId());
 		String sales = stockItem.getTwentyTwelveSales();
 		if(sales != null && !sales.trim().equals("")) {
-			//Messy, just need last figure 
+			//Messy, just need last figure
 			//which is the total.
 			int saleQuantity = 0;
 			for(String s : sales.split(",")) {
@@ -31,17 +31,17 @@ public class SearchStockItemsDecorator extends AbstractBookmarksTableDecorator {
 		}
 
 		return info.toString();
-	}	
-	
+	}
+
 	public String getPriceForMarxism() {
 		StockItem stockItem = (StockItem)getCurrentRowObject();
 		if(stockItem.getQuantityForMarxism() != null) {
 			double priceForMarxism =  stockItem.getSellPrice().doubleValue() * stockItem.getQuantityForMarxism().longValue();
-			return currencyFormatter.print(priceForMarxism, Locale.UK);
+			return numberStyleFormatter.print(priceForMarxism, Locale.UK);
 		}
 		return "0";
 	}
-	
+
 	public String getRealQuantityForMarxism() {
 		StockItem stockItem = (StockItem)getCurrentRowObject();
 		Long quantityForMarxism = stockItem.getQuantityForMarxism() == null ? 0 : stockItem.getQuantityForMarxism().longValue();
@@ -49,56 +49,56 @@ public class SearchStockItemsDecorator extends AbstractBookmarksTableDecorator {
 		if( realQuantityForMarxism < 0 ) return "0";
 		return realQuantityForMarxism.toString();
 	}
-	
-	
+
+
 	public String getPriceMissingForMarxism() {
 		StockItem stockItem = (StockItem)getCurrentRowObject();
 		if(stockItem.getQuantityForMarxism() != null) {
 			long amountMissing = stockItem.getQuantityForMarxism().longValue() - stockItem.getQuantityInStock().longValue();
 			if(amountMissing > 0) {
 				double priceForMarxism =  stockItem.getSellPrice().doubleValue() * amountMissing;
-				return currencyFormatter.print(priceForMarxism, Locale.UK);
+				return numberStyleFormatter.print(priceForMarxism, Locale.UK);
 			}
 		}
 		return "0";
-	}	
+	}
 
 	public String getInfo() {
 		StockItem stockItem = (StockItem)getCurrentRowObject();
 		return getInfo(stockItem);
 	}
-	
+
 	public String getSellPrice() {
 		StockItem stockItem = (StockItem)getCurrentRowObject();
 		return currencyFormatter.print(stockItem.getSellPrice(), Locale.UK);
 	}
-	
+
 	public String getPublisherPrice() {
 		StockItem stockItem = (StockItem)getCurrentRowObject();
 		return getPublisherPrice(stockItem);
 	}
-	
+
 	public String getPublisherPrice(StockItem stockItem) {
 		return currencyFormatter.print(stockItem.getPublisherPrice(), Locale.UK);
 	}
-	
+
 	public String getPrices() {
 		StockItem stockItem = (StockItem)getCurrentRowObject();
 		return getPrices(stockItem);
-		
-	}	
-	
-	protected String getPrices(StockItem stockItem) {
-		return currencyFormatter.print(stockItem.getSellPrice(), Locale.UK) + "/" +
-			currencyFormatter.print(stockItem.getPublisherPrice(), Locale.UK) + "/" +
-			currencyFormatter.print(stockItem.getCostPrice(), Locale.UK);
+
 	}
-	
+
+	protected String getPrices(StockItem stockItem) {
+		return numberStyleFormatter.print(stockItem.getSellPrice(), Locale.UK) + "/" +
+			numberStyleFormatter.print(stockItem.getPublisherPrice(), Locale.UK) + "/" +
+			numberStyleFormatter.print(stockItem.getCostPrice(), Locale.UK);
+	}
+
 	public String getQuantities() {
 		StockItem stockItem = (StockItem)getCurrentRowObject();
 		Long qFm = (stockItem.getQuantityForMarxism() == null ? 0 : stockItem.getQuantityForMarxism());
-		return stockItem.getQuantityInStock() + 
-				"/" + stockItem.getQuantityOnOrder() + 
+		return stockItem.getQuantityInStock() +
+				"/" + stockItem.getQuantityOnOrder() +
 				"/" + stockItem.getQuantityForCustomerOrder() +
 				"/" + stockItem.getQuantityReadyForCustomer() +
 				"/" + qFm;
