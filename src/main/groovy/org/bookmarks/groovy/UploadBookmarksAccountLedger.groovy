1054a05d002890d61a20c0604f3e8b6239e54d8b
jbeaken <jack747@gmail.com>
2017-09-05 16:07:53 +0100

    geting upload credit notes from vt ready

--- src/main/groovy/org/bookmarks/groovy/UploadBookmarksAccountLedger.groovy
+++ src/main/groovy/org/bookmarks/groovy/UploadBookmarksAccountLedger.groovy
@@ -1,5 +1,6 @@
 // From VT :
-// Display Ledger Reports, customers, copy entire report button,
+// Display -> Ledger Reports -> Single Ledger -> customers
+// copy entire report button,
 // Run this script
 
 
@@ -7,8 +8,8 @@
 JRN 000001 appears to always be an opening account
 
 Problems :
-Customers: Ford, J Glyn								
-045488		
+Customers: Ford, J Glyn
+045488
 
 What is D?
 */
@@ -17,6 +18,8 @@ package org.bookmarks.groovy
 import groovy.transform.ToString
 import groovy.sql.Sql;
 import groovy.grape.Grape;
+
+import static java.util.Calendar.*
 
 //Load the mysql driver
 Grape.grab(group:'mysql', module:'mysql-connector-java', version:'5.1.39', classLoader: this.class.classLoader.rootLoader)
@@ -37,35 +40,35 @@ customers = []
 new File("/home/bookmarks/ledgers_club_account.txt").splitEachLine("\t") {fields ->
 
 	def firstRow = fields[0]      //This will be customer id, of blank
-	
+
 	//Blank rows
 	if(!firstRow) {
 		return
 	}
 
-	
+
 
 	if( firstRow ==~ /\d{5}/ ) {
 		//have a customer
 
-		
-		if(customer && !customer.currentBalance)  {
-			throw new Exception("Have found customer ${firstRow}, but already have a customer ${customer.id} in progress!")
-			return
-		}		
-		
+
+		// if(customer && !customer.currentBalance)  {
+		// 	throw new Exception("Have found customer ${firstRow}, but already have a customer ${customer.id} in progress!")
+		// 	return
+		// }
+
 		if(customer) customers.add( customer )
 
 		customer = new Customer(id : Long.valueOf(firstRow))
 		return
 	}
 
-	if(firstRow.startsWith("Balance at")) {
-		//found current Balance
-		println "balance at " + fields
-
-		customer.currentBalance = Float.valueOf( fields[8].replace(",", "") ) * -1
-	}	
+	// if(firstRow.startsWith("Balance at")) {
+	// 	//found current Balance
+	// 	println "balance at " + fields
+  //
+	// 	customer.currentBalance = Float.valueOf( fields[8].replace(",", "") ) * -1
+	// }
 
 	if( firstRow ==~ /\d{2}\/\d{2}\/\d{2}/ ) {
 		if(!customer)  {
@@ -87,7 +90,7 @@ new File("/home/bookmarks/ledgers_club_account.txt").splitEachLine("\t") {fields
 
 		if(fields[8]) balance = Float.valueOf( fields[8].replace(",", "") )
 		if(fields[7]) liability = Float.valueOf( fields[7].replace(",", "") )
-		if(fields[6]) asset = Float.valueOf( fields[6].replace(",", "") )		
+		if(fields[6]) asset = Float.valueOf( fields[6].replace(",", "") )
 
 		if(reference == "JRN 000001") {
 				//have an opening balance
@@ -97,45 +100,74 @@ new File("/home/bookmarks/ledgers_club_account.txt").splitEachLine("\t") {fields
 			customer.openingBalance = liability
 			return
 		}
-		
-		CreditNote cn = new CreditNote(reference : reference, balance : balance, asset : asset, liability : liability, details : details, date : date, )
-		//println cn
-		customer.creditNotes.add( cn )
+
+    def amount
+    if(liability && asset) throw new Exception("Cannot have both liability and asset")
+    if(liability) amount = liability
+    if(asset) amount = asset + -1
+
+    if(reference.contains("REC")) {
+
+		    CreditNote cn = new CreditNote( reference : reference, amount : amount, details : details, date : date )
+
+		  customer.creditNotes.add( cn )
+    }
 	}
 
 	count++
 
 	//if(count > 20) System.exit(0)
- 
+
 } //end splitEachLine
 println "Finished - Updated ${count} credit notes"
 //println customers
 
-sql.executeUpdate("delete from CreditNote")
+def cutoff = Date.parse("MM/dd/yyyy","01/01/2016")
 
 //Persist
-customers.each {c -> 
+customers.each {c ->
 	//if(c.id != 45671) return
 	println c.id
-	println c.currentBalance
-	
-	if(!customer.currentBalance) throw new Exception("Have customer with no current balance")
 
-	sql.executeUpdate("update customer set openingBalance = ?, currentBalance = ? where id = ?", [c.openingBalance, c.currentBalance, c.id])
+	//sql.executeUpdate("update customer set openingBalance = ?, currentBalance = ? where id = ?", [c.openingBalance, c.currentBalance, c.id])
 
 
 	c.creditNotes.each {cn ->
-		//println cn
+		println cn
+
+
+    def endDate = new Date()
+    endDate.set(hourOfDay: 23, minute: 59, second: 0, year: cn.date[YEAR], month: cn.date[MONTH], date: cn.date[DATE])
+
+    def startDate = endDate - 1
+
+    println cn.date
+    println startDate
+    println endDate
+
+    def existing = sql.firstRow("select date from CreditNote where customer_id = ? and date between ? and ?", [ c.id, startDate, endDate])
+
+    if(!existing) {
+      existing = sql.firstRow("select date from CreditNote where customer_id = ? and date = ?", c.id, startDate)
+    }
+
+    if(existing) {
+      println "************ Skipping as ${cn.date} already exists : ${existing}"
+      return
+    }
+
 		sql.executeInsert("insert into CreditNote (creationDate, customer_id, date, transactionReference, transactionDescription, amount) values (now(), ?, ?, ?, ?, ?)",
-				[c.id, cn.date, cn.reference, cn.details, cn.amount])
+				[ c.id, cn.date, cn.reference, cn.details, cn.amount])
 	}
+
+  if(c.id == 30273) System.exit(0);
 }
 
 @ToString(includeNames=true)
 class Customer {
 	Long id
 	Float openingBalance = 0.0
-	Float currentBalance 
+	Float currentBalance
 	def creditNotes = []
 }
 
@@ -144,14 +176,7 @@ class CreditNote {
 	Date date
 	String details
 	String reference
-	Float asset
-	Float liability
-	Float balance
-
-	Float getAmount() {
-		if(asset || asset == 0) return asset * -1
-		return liability;
-	}
+	Float amount
 }
 
 /*
