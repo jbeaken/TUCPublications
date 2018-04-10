4625673595c1e25c921760fc7a2144c5a243c5c6
jbeaken <jack747@gmail.com>
2018-04-10 12:08:29 +0100

    Adding JoinTable for reading list, bumping version to 3.0

--- src/main/java/org/bookmarks/domain/ReadingList.java
+++ src/main/java/org/bookmarks/domain/ReadingList.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToMany;
 import javax.persistence.OrderColumn;
@@ -17,19 +19,22 @@ import javax.validation.constraints.Size;
 @Entity
 @Table(name="reading_list")
 public class ReadingList extends AbstractEntity {
-	
+
 	@NotNull
 	@Column(unique=true)
 	@Size(min=1, max = 255)
 	private String name;
 
 	@ManyToMany(cascade={javax.persistence.CascadeType.PERSIST, javax.persistence.CascadeType.MERGE}, fetch=FetchType.EAGER)
+	@JoinTable(name="reading_list_stockitem",
+    joinColumns={@JoinColumn(name="reading_list_id", referencedColumnName="id")},
+    inverseJoinColumns={@JoinColumn(name="stockItems_id", referencedColumnName="id")})
 	@OrderColumn(name="stockItem_idx")
 	private List<StockItem> stockItems;
-	
+
 	@Column(name="is_on_website") private Boolean isOnWebsite = Boolean.TRUE;
-	@Column(name="is_on_sidebar") private Boolean isOnSidebar = Boolean.FALSE;	
-	
+	@Column(name="is_on_sidebar") private Boolean isOnSidebar = Boolean.FALSE;
+
 	public List<StockItem> getStockItems() {
 		return stockItems;
 	}
