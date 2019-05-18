702dc35ed853d124b5d8cbf07cd9cf4915a70607
jbeaken <jack747@gmail.com>
2019-05-18 21:30:11 +0100

    Removed sudo from script

--- src/main/scripts/install-tomcat.sh
+++ src/main/scripts/install-tomcat.sh
@@ -3,27 +3,25 @@
 # Variables
 TOMCATVERSION=9.0.20
 
-wget -P /tmp http://apache.mirror.anlx.net/tomcat/tomcat-9/v$TOMCATVERSION/bin/apache-tomcat-$TOMCATVERSION.tar.gz
+# wget -P /tmp http://apache.mirror.anlx.net/tomcat/tomcat-9/v$TOMCATVERSION/bin/apache-tomcat-$TOMCATVERSION.tar.gz
 
 # Extract
-sudo tar xf /tmp/apache-tomcat-$TOMCATVERSION.tar.gz -C /opt
+tar xf /tmp/apache-tomcat-$TOMCATVERSION.tar.gz -C /opt
 
 # Prepare
-sudo rm -rf /opt/apache-tomcat-$TOMCATVERSION/webapps
-sudo mkdir /opt/apache-tomcat-$TOMCATVERSION/webapps
-sudo cp /home/git/paper/scripts/server.ajp.xml /opt/apache-tomcat-$TOMCATVERSION/conf/server.xml
-sudo cp /home/git/paper/scripts/context.xml /opt/apache-tomcat-$TOMCATVERSION/conf/context.xml
-sudo cp /home/git/paper/scripts/setenv.sh /opt/apache-tomcat-$TOMCATVERSION/bin/setenv.sh
+rm -rf /opt/apache-tomcat-$TOMCATVERSION/webapps
 
-# Paper app
-sudo cp /opt/tomcat/webapps/ROOT.war /opt/apache-tomcat-$TOMCATVERSION/webapps/ROOT.war
+cp /opt/tomcat/conf/server.xml /opt/apache-tomcat-$TOMCATVERSION/conf/server.xml
+cp /opt/tomcat/bin/setenv.sh /opt/apache-tomcat-$TOMCATVERSION/bin/setenv.sh
+
+cp -r /opt/tomcat/webapps /opt/apache-tomcat-$TOMCATVERSION/webapps
 
 # Tomcat systemd
 # sudo cp /home/git/puma/src/main/etc/install/tomcat.service /lib/systemd/system/
 # systemctl enable tomcat
 
 # Re-link
-sudo rm /opt/tomcat
-sudo ln -s /opt/apache-tomcat-$TOMCATVERSION /opt/tomcat
+rm /opt/tomcat
+ln -s /opt/apache-tomcat-$TOMCATVERSION /opt/tomcat
 
-sudo systemctl restart tomcat
+systemctl restart tomcat
