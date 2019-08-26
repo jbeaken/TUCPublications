cf323650871220a9a7f260ce728291ef548453a0
jbeaken <jack747@gmail.com>
2019-08-26 11:24:33 +0100

    Replacing user ec-admin with tomcat user

--- src/main/etc/packer/install.sh
+++ src/main/etc/packer/install.sh
@@ -10,7 +10,7 @@ sudo mkdir -p /home/bookmarks/images
 mkdir ~/.aws
 
 # Extract needed files
-tar xjf stuff.tar.bz2
+tar xjf files.tar.bz2
 
 # tomcat
 sudo cp stuff/tomcat/conf/tomcat.conf /etc/tomcat/conf.d/bookmarks.conf
@@ -19,13 +19,9 @@ sudo cp stuff/tomcat/webapps/ROOT.war /usr/share/tomcat/webapps/ROOT.war
 # images
 cp stuff/aws/credentials ~/.aws/credentials
 cp stuff/aws/config ~/.aws/config
-sudo chown ec2-user:ec2-user -R /home/bookmarks/
+sudo chown tomcat:tomcat -R /home/bookmarks/
 
 # aws s3 sync s3://paper-files-bucket /home/paper/Image
 
-# sudo systemctl enable tomcat
-#sudo systemctl enable httpd
 sudo systemctl enable tomcat
 
-#sudo systemctl start httpd
-#sudo systemctl start tomcat
