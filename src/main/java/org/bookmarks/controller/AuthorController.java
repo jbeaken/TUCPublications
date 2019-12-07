41b1c0a7475837f84e16fbc2b19fa4d4be728c28
Jack Beaken <jack747@gmail.com>
2019-12-07 18:49:30 +0000

    Adding better debuging

--- src/main/java/org/bookmarks/controller/AuthorController.java
+++ src/main/java/org/bookmarks/controller/AuthorController.java
@@ -31,9 +31,9 @@ public class AuthorController extends AbstractBookmarksController<Author> {
 
 	@Autowired
 	private AuthorService authorService;
-	
+
 	private Logger logger = LoggerFactory.getLogger(AuthorController.class);
-	
+
 	@ResponseBody
     @RequestMapping(value="/autoCompleteName", method=RequestMethod.GET)
     public String autoCompleteSurname(String term, Model modelMap) {
@@ -46,47 +46,47 @@ public class AuthorController extends AbstractBookmarksController<Author> {
             String json = buffer.toString();
             json = json.substring(0, json.length() - 2) + "  ]";
             return json;
-    }	
-	
+    }
+
 	@ResponseBody
 	@RequestMapping(value="/addAuthor", method=RequestMethod.GET)
 	public String addAuthor(String name, HttpSession session) {
 		logger.debug("Adding " + name + " to stock item as new author");
-		
+
 		StockItem stockItem = (StockItem) session.getAttribute("sessionStockItem");
-		
+
 		if(stockItem == null) {
 			//Session timeout
-			return "failure"; 
+			return "failure";
 		}
-		 
+
 		Author exists = authorService.findByName(name);
 		if(exists != null) {
 			logger.debug("Author already exists");
 			return "Author already exists";
 		}
-		
+
 		Author author = new Author();
 		author.setName(name);
 //		authorService.save(author);
-		
+
 		stockItem.getAuthors().add(author);
 		logger.debug("Have added author " + author.getName() + " to " + stockItem.getTitle());
-		
+
 		return getAuthorTable(stockItem);
-	}		
-	
+	}
+
 	@ResponseBody
 	@RequestMapping(value="/removeAuthor", method=RequestMethod.GET)
 	public String removeAuthor(String name, HttpSession session, ModelMap modelMap) {
 
 		StockItem stockItem = (StockItem) session.getAttribute("sessionStockItem");
-		
+
 		if(stockItem == null) {
 			//Session timeout
-			return "failure";  
+			return "failure";
 		}
-		
+
 		Author toRemove = null;
 		for(Author a : stockItem.getAuthors()) {
 			if(a.getName().equals(name)) {
@@ -94,10 +94,10 @@ public class AuthorController extends AbstractBookmarksController<Author> {
 			}
 		}
 		stockItem.getAuthors().remove(toRemove);
-		
+
 		return getAuthorTable(stockItem);
-	}	
-	
+	}
+
 	@ResponseBody
     @RequestMapping(value="/addAuthorToStock", method=RequestMethod.GET)
 	public String addAuthorToStock(Long authorId, HttpSession session) {
@@ -106,32 +106,32 @@ public class AuthorController extends AbstractBookmarksController<Author> {
 			//Session timeout
 			return "failure";
 		}
-		
+
 		Author author = authorService.get(authorId);
 		stockItem.getAuthors().add(author);
 
 		return getAuthorTable(stockItem);
 	}
-	
+
 	@ResponseBody
-    @RequestMapping(value="/loadAuthorTable", method=RequestMethod.GET)
+  @RequestMapping(value="/loadAuthorTable", method=RequestMethod.GET)
 	public String loadAuthorTable(HttpSession session) {
 		StockItem stockItem = (StockItem) session.getAttribute("sessionStockItem");
 		if(stockItem == null) {
 			//Session timeout
 			return "failure";
 		}
-		
+
 		return getAuthorTable(stockItem);
-	}	
-	
+	}
+
 	private String getAuthorTable(StockItem stockItem) {
 		String table = "<table><th>Author</th><th>Actions</th>";
 		for(Author a : stockItem.getAuthors()) {
 			table = table + "<tr><td>" + a.getName() + "</td><td><a onclick=\"javascript:removeAuthor('" + a.getName() + "')\"> Delete</a></td></tr>";
 		}
-		table = table + "</table>";  
-		
+		table = table + "</table>";
+
 		return table;
 	}
 
@@ -153,32 +153,32 @@ public class AuthorController extends AbstractBookmarksController<Author> {
 
 		modelMap.addAttribute("authorList", categories);
 		modelMap.addAttribute("searchResultCount", authorSearchBean.getSearchResultCount());
-		
+
 		return "searchAuthors";
 	}
 
 	@RequestMapping(value="/moveAndDelete/{id}", method=RequestMethod.GET)
-	public String moveAndDelete(@PathVariable("id") Long id, ModelMap modelMap) {	
-		
+	public String moveAndDelete(@PathVariable("id") Long id, ModelMap modelMap) {
+
 		modelMap.addAttribute("authorToDeleteId", id);
-		
+
 		return "moveAndDelete";
 	}
 
 	@RequestMapping(value="/moveAndDelete", method=RequestMethod.POST)
 	public String moveAndDelete(Long authorToDeleteId, Long authorToKeepId, HttpSession session, ModelMap modelMap) {
-		
+
 		Author authorToDelete = authorService.get(authorToDeleteId);
 		Author authorToKeep = authorService.get(authorToKeepId);
-		
-		authorService.moveAndDelete(authorToDelete, authorToKeep);
-		
-//		modelMap.addAttribute(author);
-		
-		return "moveAndDelete";
-	}		
 
-	
+		authorService.moveAndDelete(authorToDelete, authorToKeep);
+
+//		modelMap.addAttribute(author);
+
+		return "moveAndDelete";
+	}
+
+
 
 	@RequestMapping(value="/displaySearch", method=RequestMethod.GET)
 	public String displaySearch(HttpSession session, HttpServletRequest request, ModelMap modelMap) {
@@ -197,19 +197,19 @@ public class AuthorController extends AbstractBookmarksController<Author> {
 			//Most likely due to this invoice being referenced from col
 			addError("Cannot delete! Perhaps this author has authored", modelMap);
 			return searchFromSession(session, request, modelMap);
-		}		
-		
-		
+		}
+
+
 		return "redirect:searchFromSession";
-	}	
+	}
 
 	@RequestMapping(value="/add", method=RequestMethod.POST)
 	public String add(@Valid Author author, BindingResult bindingResult, RedirectAttributes redirectAttributes, HttpSession session, HttpServletRequest request, ModelMap modelMap) {
-		
+
 		author.setName(author.getName().trim());
-		
+
 		Author exists = authorService.findByName(author.getName());
-		
+
 		if(exists != null) {
 			addError(author.getName() + " already exists", modelMap);
 			return "addAuthor";
@@ -218,19 +218,19 @@ public class AuthorController extends AbstractBookmarksController<Author> {
 			addError("Cannot save author", modelMap);
 			return "addAuthor";
 		}
-		
+
 		authorService.save(author);
-		
+
 		AuthorSearchBean authorSearchBean = new AuthorSearchBean();
 		authorSearchBean.setAuthor(author);
-		
-		session.setAttribute("authorSearchBean", authorSearchBean);		
-		
+
+		session.setAttribute("authorSearchBean", authorSearchBean);
+
 		redirectAttributes.addFlashAttribute("success", "Have added author " + author.getName());
-		
+
 		return "redirect:searchFromSession";
 	}
-	
+
 	@RequestMapping(value="/add", method=RequestMethod.GET)
 	public String displayAdd(ModelMap modelMap) {
 		modelMap.addAttribute(new Author());
@@ -244,7 +244,7 @@ public class AuthorController extends AbstractBookmarksController<Author> {
 			modelMap.addAttribute("flow", flow);
 			return "editAuthor";
 		}
-		
+
 		Author dbAuthor = authorService.get(author.getId());
 		dbAuthor.setName(author.getName());
 
@@ -252,7 +252,7 @@ public class AuthorController extends AbstractBookmarksController<Author> {
 
 		AuthorSearchBean authorSearchBean = new AuthorSearchBean();
 		authorSearchBean.setAuthor(author);
-		session.setAttribute("authorSearchBean", authorSearchBean);				
+		session.setAttribute("authorSearchBean", authorSearchBean);
 
 		return "redirect:searchFromSession";
 	}
@@ -269,15 +269,15 @@ public class AuthorController extends AbstractBookmarksController<Author> {
 	@RequestMapping(value="/searchFromSession")
 	public String searchFromSession(HttpSession session, HttpServletRequest request, ModelMap modelMap) {
 		AuthorSearchBean authorSearchBean = (AuthorSearchBean) session.getAttribute("authorSearchBean");
-		
+
 		if(authorSearchBean == null) {
 			authorSearchBean = new AuthorSearchBean();
 		}
-		
+
 		authorSearchBean.isFromSession(true);
-		
+
 		modelMap.addAttribute(authorSearchBean);
-		
+
 		return search(authorSearchBean, request, session, modelMap);
 	}
 
@@ -286,4 +286,3 @@ public class AuthorController extends AbstractBookmarksController<Author> {
 		return authorService;
 	}
 }
-
